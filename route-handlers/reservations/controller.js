2d68c35da478610c8f9f965fb58cdc803e3c05d0
Dario Kemenovic <dkemenovic@gmail.com>
2019-05-07 17:43:06 +0200

    Create updateReservations and deleteReservation

--- route-handlers/reservations/controller.js
+++ route-handlers/reservations/controller.js
@@ -37,9 +37,39 @@ const createReservations = (req, res, next) => {
     );
   };
 
+const updateReservations = (req, res, next) => {
+const reservation_id = req.params.id;
+const { is_reserved, reservation_start, reservation_end, hall_id } = req.body;
+
+db.query(
+    'UPDATE reservations SET is_reserved = $1, reservation_start = $2, reservation_end = $3, hall_id = $4 WHERE reservation_id = $5',
+    [is_reserved, reservation_start, reservation_end, hall_id],
+    (err, results) => {
+    if (err) {
+        throw err;
+    }
+    res.status(200).send(results.rows);
+    }
+);
+};
+
+const deleteReservation = (req, res, next) => {
+const reservation_id = req.params.id;
+
+db.query(
+    'DELETE FROM reservations WHERE reservation_id = $1', [reservation_id], (err, results) => {
+    if (err) {
+    throw err;
+    }
+    res.status(200).send(results.rows);
+});
+};
+
 
 module.exports = {
     getReservations,
     getreservationsByid,
-    createReservations
+    createReservations,
+    updateReservations,
+    deleteReservation
 }
--- route-handlers/reservations/index.js
+++ route-handlers/reservations/index.js
@@ -5,5 +5,7 @@ const reservationsController = require('./controller');
 router.route("/").get(reservationsController.getReservations);
 router.route("/:id").get(reservationsController.getreservationsByid);
 router.route("/create").post(reservationsController.createReservations);
+router.route("/edit/:id").put(reservationsController.updateReservations);
+router.route("/delete/:id").delete(reservationsController.deleteReservations);
 
 module.exports = router;
