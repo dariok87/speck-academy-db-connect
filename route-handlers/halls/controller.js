782c9676b9b73329c7c5efb704ad0f19f98a7275
Dario Kemenovic <dkemenovic@gmail.com>
2019-05-07 03:53:26 +0200

    Define getHallbyId method

--- route-handlers/halls/controller.js
+++ route-handlers/halls/controller.js
@@ -1,14 +1,26 @@
 const db = require("../../db/connect");
 
 const getHalls = (req, res, next) => {
-    db.query("SELECT * FROM halls", (err, result) => {
-        if (err) {
-            return  next(err);
-        } 
-        res.status(200).send(result.rows);
-    });
-}
+  db.query("SELECT * FROM halls", (err, result) => {
+    if (err) {
+      return  next(err);
+    } 
+    res.status(200).send(result.rows);
+  });
+};
+
+const getHallByid = (req, res, next) => {
+  const hall_id = req.params.id;
+  
+  db.query('SELECT * from halls where hall_id = $1', [hall_id], (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).send(result.rows);
+  });
+};
 
 module.exports = {
-  getHalls
+  getHalls,
+  getHallByid
 }
--- route-handlers/halls/index.js
+++ route-handlers/halls/index.js
@@ -3,5 +3,6 @@ const router = new express.Router();
 const hallsController = require('./controller');
 
 router.route("/").get(hallsController.getHalls);
+router.route("/:id").get(hallsController.getHallByid);
 
 module.exports = router;
